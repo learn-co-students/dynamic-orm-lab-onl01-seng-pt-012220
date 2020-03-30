be68032281e9ddb1ac432e237a6686322a833550
Charles Pelton <charliejp0311@gmail.com>
2020-03-30 17:15:12 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,16 +17,36 @@ class InteractiveRecord
       column_names.compact
     end
 
-    self.column_names.each do |col_name|
-      # binding.pry
-      attr_accessor col_name.to_sym
-    end
-
-    def initialzie(attributes = {})
+    def initialize(attributes = {})
       # binding.pry
       attributes.each do |prop, val|
-        self.send("#{prop}=", value)
+        self.send("#{prop}=", val)
       end
+    end
+
+    def table_name_for_insert
+      self.class.table_name
+    end
+
+    def col_names_for_insert
+      self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    end
+
+    def values_for_insert
+      values = []
+      self.class.column_names.each do |col_name|
+        values << "'#{send(col_name)}'" unless send(col_name).nil?
+      end
+      values.join(", ")
+    end
+
+    def save
+      sql = <<-SQL
+        INSERT INTO #{self.class.table_name} (#{self.col_names_for_insert})
+        VALUES (#{self.values_for_insert})
+      SQL
+      DB[:conn].execute(sql)
+      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.class.table_name}")[0][0]
     end
 
     def self.find_by_name(name)
@@ -35,9 +55,10 @@ class InteractiveRecord
     end
 
     def self.find_by(attribute)
+      what_to_find = nil
       attribute.each do |k,v|
-        DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE ? = ?", k, v)
+        what_to_find = DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{k.to_s} = '#{v}';")
       end
+      what_to_find
     end
-
 end
