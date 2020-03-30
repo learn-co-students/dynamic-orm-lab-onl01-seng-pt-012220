15a821a1d126a10b0048f4070fd0092564a2edce
ohjacksn <jacksonmo@me.com>
2020-03-30 10:13:58 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,7 @@ class InteractiveRecord
     end
 
     def self.column_names
-        DB[:conn].results_as_hash = true
+        # DB[:conn].results_as_hash = true
 
         sql = "PRAGMA table_info('#{table_name}')"
 
@@ -45,16 +45,22 @@ class InteractiveRecord
     end
 
     def save
-        sql = <<-SQL
-            INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
-            SQL
+        sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
         DB[:conn].execute(sql)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+
     end
 
     def self.find_by_name(name)
         sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
         DB[:conn].execute(sql)
     end
-binding.pry
+
+    def self.find_by(attribute_hash)
+        value = attribute_hash.values.first
+        formatted_value = value.class == Fixnum ? value : "'#{value}'"
+        sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+        DB[:conn].execute(sql)
+    end 
+# binding.pry
 end
